799b22f29bcfd361c7b8b28adcec1ceb1886e94d
Umut Hope YILDIRIM <umut475@gmail.com>
2022-11-14 18:10:47 -0500

    stop googling things

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -1,12 +1,13 @@
 class JsonWebToken
     class << self
+        SECRET_KEY = ENV["RAILS_ENV"] == "production" ? ENV["SECRET_KEY_BASE"] : Rails.application.secrets.secret_key_base.to_s
         def encode(payload, exp = 24.hours.from_now)
             payload[:exp] = exp.to_i
-            JWT.encode(payload, Rails.application.secrets.secret_key_base)
+            JWT.encode(payload, SECRET_KEY)
         end
         
         def decode(token)
-            body = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
+            body = JWT.decode(token, SECRET_KEY)[0]
             HashWithIndifferentAccess.new body
         rescue
             nil
